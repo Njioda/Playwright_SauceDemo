7081bb021a2a39d3e14b3afaf36a0661228b6da6
Njioda <nnjioda@yahoo.fr>
2026-04-13 00:12:48 +0200

    neue2

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,32 +1,27 @@
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from pages.cart_page import CartPage
-from playwright.sync_api import sync_playwright
 
 
 def test_add_to_cart_and_verify(page):
-    
-        login_page = LoginPage(page)
-        inventory_page = InventoryPage(page)
-        login_page.navigate()
-        login_page.login("standard_user", "secret_sauce")
 
-        # Wait for inventory page
-        inventory_page.wait_until_loaded()
-        assert inventory_page.is_loaded()
+    # 🔐 Login
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.login("standard_user", "secret_sauce")
 
-        # Inventory
-        inventory = InventoryPage(page)
-        inventory.wait_until_loaded()
+    # 📦 Inventory
+    inventory = InventoryPage(page)
+    inventory.wait_until_loaded()
+    assert inventory.is_loaded()
 
-        #Produkt hinzufügen
-        inventory.add_first_item_to_cart()
+    # 🛒 Produkt hinzufügen
+    inventory.add_first_item_to_cart()
 
-        #Warenkorb öffnen
-        cart = CartPage(page)
-        cart.open_cart()
-        cart.wait_until_loaded()
+    # 🧺 Cart
+    cart = CartPage(page)
+    cart.open_cart()
+    cart.wait_until_loaded()
 
-        #Validierung
-        assert cart.get_cart_count() == 1
-        
+    # ✅ Validierung
+    assert cart.get_cart_count() == 1
